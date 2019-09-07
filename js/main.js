bd2653037b65699b55cf90f094717eaaa8a3b31d
Ruckaiya <ruckaiya.awf5@gmail.com>
2019-09-08 02:06:00 +0600

    f

---
+++ js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  $("#toggle").click(function(){
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+  });
+
+  $("#resize ul li a").click(function(){
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+  });
+
+  $(".close-btn").click(function(){
+    $(this).toggleClass('on');
+    $("#resize").toggleClass("active");
+  });
+
+
+$(".read").click(function(){
+  $(this).prev().toggle();
+  $(this).siblings('.dots').toggle();
+  if($(this).text()=='Read more'){
+    $(this).text('Read less');
+  }
+  else{
+    $(this).text('Read more');
+  }
+});
+
+
+  TweenMax.from("#brand", 1, {
+    delay: 0.4,
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom("#menu li a", 1, {
+    delay: 0.4,
+    opacity: 0,
+    ease: Expo.easeInOut
+}, 0.1);
+
+// nav animation ends
+
+new WOW().init();
+
+});
